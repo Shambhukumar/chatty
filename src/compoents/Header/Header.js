f3d2cfcc04a003735f336ccad14a88c04b3f4118
Shambhu Sahu <shambhukumar.shau@gmail.com>
2022-01-20 12:40:08 +0530

    added Header

--- src/compoents/Header/Header.js
+++ src/compoents/Header/Header.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
+import "./header.scss";
+
+import Notification from "../../Assets/Svg/Notification.svg";
+import Filter from "../../Assets/Svg/Filter.svg";
+import HeaderPerson from "../../Assets/Image-Placeholder/headerPerson.jpg";
 
 const Header = () => {
-  return (<div>
-      <h1>Header</h1>
-  </div>);
+  return (
+    <div className="header">
+      <div className="header-left">
+        <span className="header-left-Text">Chat</span>
+        <div className="header-left-Filter">
+          <img src={Filter} alt="Filter Svg" /> <span>Sort</span>
+        </div>
+        </div>
+
+        <div className="header-account">
+          
+            <img src={Notification} alt="Notification Svg" />
+          
+          <div className="header-account-info">
+            <img src={HeaderPerson} alt="profile pic"/><span className="header-account-info-name">Prakash</span> 
+            <span className="header-account-info-arrow"></span>
+          </div>
+        </div>
+     
+    </div>
+  );
 };
 
 export default Header;
